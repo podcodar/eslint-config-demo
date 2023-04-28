b51337ad8a4efcdac091fb7241375a407649fa53
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-27 21:02:09 -0300

    Add eslint scripts + indent rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
+  "env": {
+    "browser": true,
+    "es2021": true,
+    "node": true
+  },
+  "overrides": [
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": ["error", 2]
+  }
 }
